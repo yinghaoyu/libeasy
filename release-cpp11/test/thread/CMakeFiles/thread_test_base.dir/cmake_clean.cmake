9fa569e39e2f3b428e196613c14fe047890508e3
rain <20520@126.com>
2022-12-09 13:11:34 +0800

    update .gitignore

--- release-cpp11/test/thread/CMakeFiles/thread_test_base.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/thread_test_base.dir/easy_uthread_test.c.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/thread_test_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
